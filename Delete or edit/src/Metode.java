3d1a3d1f19552d4980ba862ca5f9770240ef1389
Surya Laksana <suryalaksana05@gmail.com>
2025-12-07 19:37:33 +0800

    tes

---
+++ Delete or edit/src/Metode.java
@@ -0,0 +1,115 @@
+public class Metode {
+    private Node front;  
+    private Node rear;  
+    private Node top;  
+
+    private class Node {
+        Task data;
+        Node next;
+
+        Node(Task data) {
+            this.data = data;
+            this.next = null;
+        }
+    } 
+
+    public Metode() {
+        front = rear = top = null;
+    }
+
+    boolean isEmptyQueue() {
+        return front == null;
+    }
+
+    boolean isEmptyStack() {
+        return top == null;
+    }
+
+    void enqueue(Task task) {
+        Node newNode = new Node(task);
+
+        if (isEmptyQueue()) {
+            front = rear = newNode;
+        } else {
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
+
+    Task dequeue() {
+        if (isEmptyQueue()) return null;
+
+        Task task = front.data;
+        front = front.next;
+
+        if (front == null) {
+            rear = null;
+        }
+
+        return task;
+    }
+
+    void display() {
+        if (isEmptyQueue()) {
+            System.out.println("Tidak ada tugas.");
+            return;
+        }
+
+        Node temp = front;
+        while (temp != null) {
+            temp.data.display();
+            temp = temp.next;
+        }
+    }
+
+    Task getById(int id) {
+        Node temp = front;
+        while (temp != null) {
+            if (temp.data.id == id) {
+                return temp.data;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    Task removeById(int id) {
+        if (isEmptyQueue()) return null;
+
+        Node temp = front;
+        Node prev = null;
+
+        while (temp != null) {
+            if (temp.data.id == id) {
+                if (prev == null) {
+                    front = temp.next;
+                } else {
+                    prev.next = temp.next;
+                }
+
+                if (temp == rear) {
+                    rear = prev;
+                }
+
+                return temp.data;
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    void push(Task task) {
+        Node newNode = new Node(task);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    Task pop() {
+        if (isEmptyStack()) return null;
+
+        Task task = top.data;
+        top = top.next;
+        return task;
+    }
+}
